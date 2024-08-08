b74620d511925969540024041940816870fba93b
jathomson <jathomson@deloitte.com.au>
2024-08-08 19:39:53 +1000

    Add UniqueIpCount implementation

--- LogFileReaderLibrary/Services/LogFileAnalyserService.cs
+++ LogFileReaderLibrary/Services/LogFileAnalyserService.cs
@@ -1,4 +1,7 @@
-﻿namespace LogFileReaderLibrary.Services;
+﻿using System.ComponentModel.DataAnnotations;
+using LogFileReaderLibrary.Helpers;
+
+namespace LogFileReaderLibrary.Services;
 
 /// <summary>
 /// This service analyses the contents of a log file.
@@ -7,7 +10,16 @@ public class LogFileAnalyserService
 {
     public int UniqueIpCount(Stream logContent)
     {
-        throw new NotImplementedException();
+        var hashSet = new HashSet<string>();
+        using var reader = new StreamReader(logContent);
+        
+        while (reader.ReadLine() is { } line)
+        {
+            var logEntry = HttpRequestLogEntryDeserializer.DeserializeApacheClf(line);
+            hashSet.Add(logEntry.IpAddress);
+        }
+
+        return hashSet.Count;
     }
 
     public IDictionary<string, int> MostVisitedUrls(Stream logContent, int top)
