55d7934a00657d32f20214be498b3891d24fae15
jathomson <jathomson@deloitte.com.au>
2024-08-08 16:16:12 +1000

    Add log file skeleton with class library unit tests

---
+++ LogFileReaderLibrary/Services/LogFileAnalyserService.cs
@@ -0,0 +1,22 @@
+﻿namespace LogFileReaderLibrary.Services;
+
+/// <summary>
+/// This service analyses the contents of a log file.
+/// </summary>
+public class LogFileAnalyserService
+{
+    public int UniqueIpCount(Stream logContent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDictionary<string, int> MostVisitedUrls(Stream logContent, int top)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDictionary<string, int> MostActiveIps(Stream logContent, int top)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LogFileReaderLibraryTests/ServiceTests/LogFileAnalyserServiceTests.cs
@@ -0,0 +1,53 @@
+using CommonLibrary.Helpers;
+using FluentAssertions;
+using LogFileReaderLibrary.Services;
+
+namespace LogFileReaderLibraryTests.ServiceTests;
+
+public class LogFileAnalyserServiceTests
+{
+    [Theory]
+    [InlineData("TestData.SampleLogFile.log", 11)]
+    public void UniqueIpCountReturnsCorrectCount(string testResourceName, int expectedCount)
+    {
+        // Arrange
+        var testDataStream = testResourceName.ReadEmbeddedResourceAsStream();
+        var analyser = new LogFileAnalyserService();
+        
+        // Act
+        var actualCount = analyser.UniqueIpCount(testDataStream);
+
+        // Assert
+        actualCount.Should().Be(expectedCount);
+    }
+    
+    [Theory]
+    [InlineData("TestData.SampleLogFile.log", 3, "/docs/manage-websites/", "/faq/", "/intranet-analytics/")]
+    public void MostVisitedUrlsReturnsCorrectUrls(string testResourceName, int top, params string[] expectedTopUrls)
+    {
+        // Arrange
+        var testDataStream = testResourceName.ReadEmbeddedResourceAsStream();
+        var analyser = new LogFileAnalyserService();
+        
+        // Act
+        var actualTopUrls = analyser.MostVisitedUrls(testDataStream, top).Keys.ToArray();
+
+        // Assert
+        actualTopUrls.Should().Equal(expectedTopUrls);
+    }
+    
+    [Theory]
+    [InlineData("TestData.SampleLogFile.log", 3, "168.41.191.40", "177.71.128.21", "50.112.00.11")]
+    public void MostActiveIpsReturnsCorrectIps(string testResourceName, int top, params string[] expectedTopIps)
+    {
+        // Arrange
+        var testDataStream = testResourceName.ReadEmbeddedResourceAsStream();
+        var analyser = new LogFileAnalyserService();
+        
+        // Act
+        var actualTopUrls = analyser.MostActiveIps(testDataStream, top).Keys.ToArray();
+
+        // Assert
+        actualTopUrls.Should().Equal(expectedTopIps);
+    }
+}
