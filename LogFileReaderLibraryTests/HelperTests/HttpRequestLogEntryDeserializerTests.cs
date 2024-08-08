4cf40f2d2055a3da1630231ec03d7ac984235814
jathomson <jathomson@deloitte.com.au>
2024-08-09 00:03:18 +1000

    Add bad pattern matching test

--- LogFileReaderLibraryTests/HelperTests/HttpRequestLogEntryDeserializerTests.cs
+++ LogFileReaderLibraryTests/HelperTests/HttpRequestLogEntryDeserializerTests.cs
@@ -53,7 +53,7 @@ public class HttpRequestLogEntryDeserializerTests
     [InlineData("177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"")]
     [InlineData("72.44.32.10 - - [09/Jul/2018:15:48:07 +0200] \"GET / HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0\" junk extra")]
     [InlineData("168.41.191.9 - - [09/Jul/2018:22:56:45 +0200] \"GET /docs/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; Linux i686; rv:6.0) Gecko/20100101 Firefox/6.0\" 456 789")]
-    public void SuccessfulLogEntryDeserializationGeneric(string logEntryRaw)
+    public void SuccessfulLogEntryDeserialization(string logEntryRaw)
     {
         // Act
         var logEntry = HttpRequestLogEntryDeserializer.DeserializeApacheClf(logEntryRaw);
@@ -74,5 +74,13 @@ public class HttpRequestLogEntryDeserializerTests
             logEntry.Referer.Should().NotBeNull();
             logEntry.UserAgent.Should().NotBeNull();
         }
+    }
+
+    [Theory]
+    [InlineData("This is not a log.")]
+    public void FailLogEntryDeserialization(string logEntryRaw)
+    {
+        // Act and Assert
+        var exception = Assert.Throws<FormatException>(() => HttpRequestLogEntryDeserializer.DeserializeApacheClf(logEntryRaw));
     }
 }
