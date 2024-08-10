a4b74fdb15f4878934e5d97b5cf2e221494ef0c1
jathomson <jathomson@deloitte.com.au>
2024-08-10 19:56:42 +1000

    Add xml docs for LogFileAnalyserHandler.cs

--- LogFileReaderConsoleApp/Handlers/LogFileAnalyserHandler.cs
+++ LogFileReaderConsoleApp/Handlers/LogFileAnalyserHandler.cs
@@ -3,8 +3,17 @@ using LogFileReaderLibrary.Services;
 
 namespace LogFileReaderConsoleApp.Handlers;
 
+/// <summary>
+/// Provides methods to analyze log files and report insights.
+/// </summary>
 public static class LogFileAnalyserHandler
 {
+    /// <summary>
+    /// Analyzes the provided log entries and reports insights such as unique IP count, 
+    /// most visited URLs, and most active IPs.
+    /// </summary>
+    /// <param name="logEntries">A list of log entries to analyze.</param>
+    /// <param name="top">The number of top entries to report for most visited URLs and most active IPs. Default is 3.</param>
     public static void ReportInsights(List<ApacheClfLogEntry> logEntries, int top = 3)
     {
         var uniqueIpCount = LogFileAnalyserService.UniqueIpCount(logEntries);
