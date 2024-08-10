e4624a2ce77ac335805fcd4f1c25f1a9f5d1ae68
jathomson <jathomson@deloitte.com.au>
2024-08-10 19:53:44 +1000

    Add xml docs for FileHandler.cs

--- LogFileReaderConsoleApp/Handlers/FileHandler.cs
+++ LogFileReaderConsoleApp/Handlers/FileHandler.cs
@@ -4,8 +4,17 @@ using LogFileReaderLibrary.Models.Exceptions;
 
 namespace LogFileReaderConsoleApp.Handlers;
 
+/// <summary>
+/// Provides methods for handling file operations related to Apache Common Log Format (CLF) log entries.
+/// </summary>
 public static class FileHandler
 {
+    /// <summary>
+    /// Attempts to read an Apache CLF log file and deserialize its content into a list of <see cref="ApacheClfLogEntry"/> objects.
+    /// </summary>
+    /// <param name="filePath">The path to the log file to be read.</param>
+    /// <param name="logEntries">When this method returns, contains the list of deserialized <see cref="ApacheClfLogEntry"/> objects if the operation is successful; otherwise, an empty list.</param>
+    /// <returns><c>true</c> if the file was successfully read and deserialized; otherwise, <c>false</c>.</returns>
     public static bool TryReadFile(string filePath, out List<ApacheClfLogEntry> logEntries)
     {
         logEntries = [];
