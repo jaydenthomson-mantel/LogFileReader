7177ec50da0686aaf8bd4bcba5477bd3146e8784
jathomson <jathomson@deloitte.com.au>
2024-08-10 19:45:56 +1000

    Refactor model to remove warnings

--- LogFileReaderLibrary/Models/HttpRequestLogEntry.cs
+++ LogFileReaderLibrary/Models/HttpRequestLogEntry.cs
@@ -4,15 +4,15 @@ namespace LogFileReaderLibrary.Models;
 
 public class HttpRequestLogEntry
 {
-    public string IpAddress { get; set; }
-    public string Identd { get; set; }
-    public string UserId { get; set; }
-    public DateTimeOffset Timestamp { get; set; }
-    public HttpMethod HttpMethod { get; set; }
-    public string HttpVersion { get; set; }
-    public Uri Resource { get; set; }
-    public HttpStatusCode StatusCode { get; set; }
-    public int ResponseSize { get; set; }
-    public string Referer { get; set; }
-    public string UserAgent { get; set; }
+    public required string IpAddress { get; init; }
+    public required string Identd { get; init; }
+    public required string UserId { get; init; }
+    public required DateTimeOffset Timestamp { get; init; }
+    public required HttpMethod HttpMethod { get; init; }
+    public required string HttpVersion { get; init; }
+    public required Uri Resource { get; init; }
+    public required HttpStatusCode StatusCode { get; init; }
+    public required int ResponseSize { get; init; }
+    public required string Referer { get; init; }
+    public required string UserAgent { get; init; }
 }
