b8ffe68c813e235d230964d7f4fee09551c660b5
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2024-09-17 14:36:15 +1000

    Update directories

--- LogFileReaderLibrary/Models/Exceptions/ApacheClfLogValidationException.cs
+++
@@ -1,7 +0,0 @@
-namespace LogFileReaderLibrary.Models.Exceptions;
-
-public class ApacheClfLogValidationException(string logLine, IEnumerable<Exception> innerExceptions)
-    : AggregateException(innerExceptions)
-{
-    public string InvalidApacheClfLogLineErrorMessage => $"One or more properties in the log line '{logLine}' were invalid.";
-}
