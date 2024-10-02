c86b3005fc9bb15cdab41c66e71a1739143c40a8
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2024-10-02 16:55:12 +1000

    Refactor FileHandlerTests.cs to add test for TryReadFile method when file does not exist and when file is bad.

--- tests/LogFileReaderConsoleAppTests/FileHandlerTests.cs
+++ tests/LogFileReaderConsoleAppTests/FileHandlerTests.cs
@@ -6,11 +6,39 @@ namespace LogFileReaderConsoleAppTests;
 
 public class FileHandlerTests
 {
-    [Theory]
-    [InlineData("file-does-not-exist.log")]
-    [InlineData("../../../TestData/BadLogFile.log")]
-    public void TryReadFile_ReturnsFalse(string filePath)
+    [Fact]
+    public void TryReadFile_ReturnsFalse_FileDoesNotExist()
     {
+        // Arrange
+        var filePath = "file-does-not-exist.log";
+        var originalOut = Console.Out;
+        using var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        try
+        {
+            // Act
+            var result = FileHandler.TryReadFile(filePath, out var logEntries);
+
+            // Assert
+            result.Should().BeFalse();
+            logEntries.Should().BeEmpty();
+            var output = stringWriter.ToString();
+            output.Should().Contain($"File not found: {filePath}");
+        }
+        finally
+        {
+            // Reset the console output to its original value
+            Console.SetOut(originalOut);
+        }
+    }
+
+    [Fact]
+    public void TryReadFile_ReturnsFalse_BadFile()
+    {
+        // Arrange
+        var filePath = "../../../TestData/BadLogFile.log";
+
         // Act
         var result = FileHandler.TryReadFile(filePath, out var logEntries);
 
