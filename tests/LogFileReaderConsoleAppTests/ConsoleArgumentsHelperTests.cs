543e0a121947ecc8087642960b21f58700c6a43e
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2024-10-02 16:46:26 +1000

    Refactor ConsoleArgumentsHelperTests.cs to set the original out back after setting it temporarily to a string writer

--- tests/LogFileReaderConsoleAppTests/ConsoleArgumentsHelperTests.cs
+++ tests/LogFileReaderConsoleAppTests/ConsoleArgumentsHelperTests.cs
@@ -20,21 +20,31 @@ public class ConsoleArgumentsHelperTests
         filePath.Should().Be("testfile.log");
     }
 
+
     [Fact]
     public void TryGetFilePath_WithNoArgs_ReturnsFalse()
     {
         // Arrange
         var args = Array.Empty<string>();
+        var originalOut = Console.Out;
         using var stringWriter = new StringWriter();
         Console.SetOut(stringWriter);
 
-        // Act
-        var result = ConsoleArgumentsHelper.TryGetFilePath(args, out var filePath);
+        try
+        {
+            // Act
+            var result = ConsoleArgumentsHelper.TryGetFilePath(args, out var filePath);
 
-        // Assert
-        result.Should().BeFalse();
-        filePath.Should().BeNull();
-        var output = stringWriter.ToString();
-        output.Should().Contain("Provide a file path.");
+            // Assert
+            result.Should().BeFalse();
+            filePath.Should().BeNull();
+            var output = stringWriter.ToString();
+            output.Should().Contain("Provide a file path.");
+        }
+        finally
+        {
+            // Reset the console output to its original value
+            Console.SetOut(originalOut);
+        }
     }
 }
