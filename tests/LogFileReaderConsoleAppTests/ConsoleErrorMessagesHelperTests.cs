fe338336d4a7c7cc926142ba7743345afc100bca
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2024-10-02 16:36:03 +1000

    Add ConsoleErrorMessagesHelperTests to test expection formatting.

---
+++ tests/LogFileReaderConsoleAppTests/ConsoleErrorMessagesHelperTests.cs
@@ -0,0 +1,32 @@
+using FluentAssertions;
+using LogFileReaderConsoleApp.Helpers;
+using LogFileReaderLibrary.Models.Exceptions;
+using Xunit;
+
+namespace LogFileReaderConsoleAppTests;
+
+public class ConsoleErrorMessagesHelperTests
+{
+    [Fact]
+    public void GetBadApacheClfFileExceptionMessage_ReturnsExpectedMessage()
+    {
+        // Arrange
+        var ex = new BadApacheClfFileException(
+        [
+            new ApacheClfLogValidationException("Invalid Apache CLF log line",
+            [
+                new("Inner exception message")
+            ])
+        ]);
+
+        var expectedMessage = "One or more exceptions occured attempting to deserialize an Apache clf file. See the inner exceptions for more details.\n" +
+                      "\tOne or more properties in the log line 'Invalid Apache CLF log line' were invalid.\n" +
+                      "\t\tInner exception message\n";
+
+        // Act
+        var result = ConsoleErrorMessagesHelper.GetBadApacheClfFileExceptionMessage(ex);
+
+        // Assert
+        result.Should().Be(expectedMessage);
+    }
+}
